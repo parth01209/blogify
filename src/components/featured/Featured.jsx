82005b81724522fe57c4cd8e85e3d8c8841619b6
parth01209 <parthprabhune12@gmail.com>
2023-09-28 19:12:00 +0530

    Second commit

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,15 +6,15 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey, Pearl here!</b> Discover my stories and creative ideas.
+        <b>Hey, Parth here!</b> Discover my stories and creative ideas.
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
-          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          <Image src="/front-image.png" alt="" fill className={styles.image} />
         </div>
         <div className={styles.textContainer}>
           <h1 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet alim consectetur adipisicing elit.
+            This blog is especially created as an assignment for Medhavyn
           </h1>
           <p className={styles.postDesc}>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.info}>
         <div className={styles.logo}>
-          <Image src="/logo.png" alt="blogify" width={50} height={50} />
+          <Image src="/profile.png" alt="blogify" width={50} height={50} />
           <h1 className={styles.logoText}>blogify</h1>
         </div>
         <p className={styles.desc}>
